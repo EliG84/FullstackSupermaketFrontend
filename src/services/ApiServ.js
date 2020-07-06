9106f22176268d27122da7f61f82f6dcafacefc3
Eli <elionlink@gmail.com>
2020-07-06 20:42:53 +0300

    06072020

--- src/services/ApiServ.js
+++ src/services/ApiServ.js
@@ -1,12 +1,12 @@
 export const apiGet = async () => {
-  const url = 'https://speedtechstore1.herokuapp.com/api';
+  const url = 'https://aqueous-brook-65256.herokuapp.com/api';
   let resp = await fetch(url);
   let data = await resp.json();
   return data;
 };
 
 export const apiGetCat = async (cat) => {
-  const url = `https://speedtechstore1.herokuapp.com/api/${cat}`;
+  const url = `https://aqueous-brook-65256.herokuapp.com/api/${cat}`;
   let resp = await fetch(url);
   let data = await resp.json();
   return data;
@@ -14,7 +14,7 @@ export const apiGetCat = async (cat) => {
 
 export const apiSearch = async (searchQ) => {
   console.log(searchQ);
-  const url = `https://speedtechstore1.herokuapp.com/${searchQ}`;
+  const url = `https://aqueous-brook-65256.herokuapp.com/api/${searchQ}`;
   let resp = await fetch(url);
   let data = await resp.json();
   console.log(data);
